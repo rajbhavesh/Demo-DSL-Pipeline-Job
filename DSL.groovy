7bb6a874981cc009e2c8ba0dfcf573efd6af865c
rajbhavesh <58482189+rajbhavesh@users.noreply.github.com>
2019-12-06 21:51:25 +0530

    Update DSL.groovy

--- DSL.groovy
+++ DSL.groovy
@@ -1,10 +1,11 @@
 pipelineJob('Job-KubeadmCreation') {
+     description('This is the job for creating a single node kubernetes cluster')
     definition {
         cpsScm {
             scm {
-                git('https://github.com/rajbhavesh/Demo.git')
+                git('https://github.com/rajbhavesh/Demo.git', 'test')
             }
-            scriptPath('demo/jenkinsfile')
+            scriptPath('jenkinsfile')
         }
     }
 }
